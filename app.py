56416319e8b30bc97777fda74118360de5ef8dfb
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2022-11-04 11:56:34 +0200

    Refactoring

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get('/api')
 async def api():
     return {'message': 'I am API response, hello!'}
 
+
 @app.get("/")
 async def index():
-        return {'message': 'Hello from FastAPI'}
+    return {'message': 'Hello from FastAPI'}
